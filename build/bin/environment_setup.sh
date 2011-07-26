4b619aa50ed947bcf9be8f1a8ab9a5792dd59c9e
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2011-07-26 12:13:32 -0300

    Writing environment_setup output to a log file.

--- build/bin/environment_setup.sh
+++ build/bin/environment_setup.sh
@@ -1,17 +1,21 @@
 source $BUILD_DIR/bin/nodeready.sh
 
-LOG="# [environment setup]"
+LOGFILE=".environment_setup.log"
+echo "# $(date)" > $LOGFILE
+
+say() { echo "# [environment setup] $*" | tee -a $LOGFILE; }
+
 if [ $NODE_VERSION ]
 then
-  echo "$LOG Using node version $NODE_VERSION"
+  say "$LOG Using node version $NODE_VERSION"
   if nvm use $NODE_VERSION | grep --silent 'not installed yet'; then
-    echo "$LOG $NODE_VERSION is not installed yet. Installing it..."
-    nvm install $NODE_VERSION
-    nvm alias default $NODE_VERSION
+    say "$LOG $NODE_VERSION is not installed yet. Installing it..."
+    nvm install $NODE_VERSION >>$LOGFILE 2>&1
+    nvm alias default $NODE_VERSION >>$LOGFILE 2>&1
   fi
 else
-  echo "$LOG Could not find NODE_VERSION environment variable. Using the latest version - already installed by nodeready."
+  say "$LOG Could not find NODE_VERSION environment variable. Using the latest version - already installed by nodeready."
 fi
 
-echo "$LOG Node version: `node --version`"
-echo "$LOG NPM version : `npm --version`"
+say "$LOG Node version: `node --version`"
+say "$LOG NPM version : `npm --version`"
